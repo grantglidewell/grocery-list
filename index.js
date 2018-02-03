1587169f42f7a704d417eaa18de8a3ede4ddd92d
Grant Glidewell <ggcadc@gmail.com>
2018-02-03 09:40:19 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mysql = require('mysql');
+const path = require('path');
+
+const server = express();
+const connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+server.use(express.static(path.resolve(__dirname, 'build')));
+
+// this should return my react page
+server.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
+server.get('/api/data/:session', (req, res) => {
+  connection.connect();
+  connection.query(`SELECT * from todos WHERE owner='${req.params.session}'`, (err, rows) => {
+    if (err) throw err;
+    return res.json(rows);
+  });
+  connection.end();
+});
+
+server.get('/api/post/:text/:session', (req, res) => {
+  connection.connect();
+  connection.query(`INSERT INTO todos (owner, todo) VALUES('${req.params.session}', '${req.params.text}')`, (err, rows) => {
+    if (err) throw err;
+    return res.json(rows);
+  });
+  connection.end();
+});
+
+
+server.get('/api/delete/:id', (req, res) => {
+  connection.connect();
+  connection.query(`DELETE FROM todos WHERE id=${req.params.id}`, (err, rows) => {
+    if (err) throw err;
+    return res.json(rows);
+  });
+  connection.end();
+});
+
+server.listen(process.env.PORT || 3001, () => {
+  console.log('listening');
+});
---
+++ src/app.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import './app.css'
+
+class App extends Component {
+  constructor(props) {
+    super();
+    this.getData = this.getData.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    
+    this.state = {
+      session: props.session,
+      input: '',
+      todos: [],
+      uri: `${process.env.NOW_URL}/api`,
+    };
+  }
+  componentDidMount() {
+    // this makes the initial fetch when the component mounts
+    this.getData()
+  }
+  getData() {
+    fetch(`${this.state.uri}/data/${this.state.session}/`).then((data, err) => {
+      if (err) console.log(err);
+      return data.json();
+    }).then((data) => {
+      // sets the state to the data value
+      this.setState({
+        todos: data,
+      });
+    });
+  }
+  deleteItem(id) {
+    fetch(`${this.state.uri}/delete/${id}`)
+      .then(() => this.getData())
+  }
+  postItem(text) {
+    // light sanatizing to remove error causing characters
+    if(text.match(new RegExp(/[-/\\^%$*+?#.()|[\]{}]/g))){
+      text = text.replace(new RegExp(/[-/\\^%$*+?#.()|[\]{}]/g), '');
+    }
+    fetch(`${this.state.uri}/post/${text}/${this.state.session}`)
+      .then(() => this.getData())
+      .then(() => this.input.value = '')
+  }
+  handleSubmit() {
+    this.setState({
+      input: this.input.value
+    }, () => this.postItem(this.state.input))
+  }
+  TodoList = () => (
+    <ul>
+      {this.state.todos.map(e => <li key={e.id}>
+        <button onClick={() => this.deleteItem(e.id)}>
+         <span role="img" aria-label="check">✔️</span>
+        </button>{e.todo}</li>)}
+    </ul>
+  )
+  InputTodo = () => (
+    <div>
+      <input 
+        type="submit" 
+        onClick={() => this.handleSubmit()} 
+        value="ToDo"
+      />
+      <input 
+        type="text" 
+        ref={(input) => this.input = input}
+        onKeyPress={(event) => event.key === 'Enter' ? this.handleSubmit(event) : null}
+      />
+    </div>
+  )
+  render() {
+    return (
+      <div className="flex">
+        <div className="input">
+          <this.InputTodo />
+        </div>
+      {this.state.todos.length ? <this.TodoList /> : <ul><li>Add Something</li></ul>}
+    </div> 
+    );
+  }
+}
+
+export default App;
