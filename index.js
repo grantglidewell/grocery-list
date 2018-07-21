4c9e90a21ab5c3e2b43a654fef7b0d8d6df3b492
Grant Glidewell <ggcadc@gmail.com>
2018-07-21 14:59:29 -0700

    🎫

--- index.js
+++ index.js
@@ -1,52 +1,55 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Todo = require('./models/todo');
-const path = require('path');
+const express = require('express')
+const mongoose = require('mongoose')
+const Todo = require('./models/todo')
+const path = require('path')
 
-const server = express();
+const server = express()
 
-server.use(express.static(path.resolve(__dirname, 'build')));
+server.use(express.static(path.resolve(__dirname, 'build')))
 
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 
-mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
+mongoose.connect(
+  process.env.MONGODB_URI,
+  { useMongoClient: true }
+)
 
 server.get('/api/data/:session', (req, res) => {
   Todo.find((err, data) => {
     if (err) {
-      console.error(err);
+      console.error(err)
     }
-    return res.jsonp(data);
-  });
-});
+    return res.jsonp(data)
+  })
+})
 
 server.get('/api/post/:text/:session', (req, res, next) => {
   const post = new Todo({
     todo: req.params.text,
-    session: req.params.session,
-  });
+    session: req.params.session
+  })
   post.save((err, postText) => {
     if (err) {
-      return next(err);
+      return next(err)
     }
-    return res.sendStatus(201, postText);
-  });
-});
+    return res.sendStatus(201, postText)
+  })
+})
 
 server.get('/api/delete/:id', (req, res) => {
-  Todo.remove({ _id: req.params.id }, (err) => {
+  Todo.remove({ _id: req.params.id }, err => {
     if (!err) {
-      res.sendStatus(200);
+      res.sendStatus(200)
     } else {
-      console.error(err);
+      console.error(err)
     }
-  });
-});
+  })
+})
 
 server.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-});
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+})
 
 server.listen(process.env.PORT || 3001, () => {
-  console.log('listening');
-});
+  console.log('listening')
+})
